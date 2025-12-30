419118ce178b0c9cbf6ada4bac2556a8549cd396
hot5656 <kyp001@yahoo.com.tw>
2025-12-30 13:41:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { LanguageProvider } from "@/contexts/LanguageContext";
+
 import Index from "./pages/Index";
 import BlogPost from "./pages/BlogPost";
 import Auth from "./pages/Auth";
@@ -16,18 +16,16 @@ const queryClient = new QueryClient();
 
 const AppRoutes = () => (
   <Routes>
-    {/* English routes */}
     <Route path="/" element={<Index />} />
     <Route path="/post/:id" element={<BlogPost />} />
     <Route path="/auth" element={<Auth />} />
     <Route path="/reset-password" element={<ResetPassword />} />
-    
-    {/* Chinese routes */}
+
     <Route path="/zh-tw" element={<Index />} />
     <Route path="/zh-tw/post/:id" element={<BlogPost />} />
     <Route path="/zh-tw/auth" element={<Auth />} />
     <Route path="/zh-tw/reset-password" element={<ResetPassword />} />
-    
+
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
@@ -38,11 +36,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <LanguageProvider>
-            <AppRoutes />
-          </LanguageProvider>
-        </BrowserRouter>
+        <AppRoutes />
         <N8nChatWidget />
       </TooltipProvider>
     </AuthProvider>
@@ -50,3 +44,4 @@ const App = () => (
 );
 
 export default App;
+
